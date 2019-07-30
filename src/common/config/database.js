656a3f96a7699c10dc51ddb36ea3bfb32926cc07
Mark He <heke@hujiang.com>
2019-07-30 22:34:21 +0800

    更新DataBase配置

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,9 +5,9 @@ module.exports = {
   database: 'shop',
   prefix: 'shop_',
   encoding: 'utf8mb4',
-  host: '47.100.20.158',
-  port: '3306',
-  user: 'acct',
-  password: '2wsxVFR$',
+  host: 'sh-cdb-iszun2gy.sql.tencentcdb.com',
+  port: '63046',
+  user: 'root',
+  password: '1qazQWER@',
   dateStrings: true
 };
